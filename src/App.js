bc4378f8e1e1d23d87cdb002c700e99600d1abbb
blueA003 <gogurye@77@naver.com>
2025-01-16 20:22:46 +0900

    made CRA

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Title from './components/Title';
+import Form from './components/Form';
+import MainCard from './components/MainCard';
 
-function App() {
+const jsonLocalStorage = {
+  setItem: (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+  getItem: (key) => {
+    return JSON.parse(localStorage.getItem(key));
+  },
+};
+
+const fetchCat = async (text) => {
+  const OPEN_API_DOMAIN = "https://cataas.com";
+  const response = await fetch(`${OPEN_API_DOMAIN}/cat/says/${text}?json=true`);
+  const responseJson = await response.json();
+  return `${OPEN_API_DOMAIN}/cat/${responseJson._id}/says/${text}`; // NOTE: API 스펙 변경으로 강의 영상과 다른 URL로 변경했습니다.
+};
+
+function CatItem(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li>
+      <img src={props.img} style={{ width: "150px" }} />
+    </li>
   );
 }
+
+function Favorites({ favorites }) {
+  if (favorites.length === 0) {
+    return <div> 사진 위 하트를 눌러 고양이 사진을 저장해봐요!</div>
+  }
+  return (
+    <ul className="favorites">
+      {favorites.map((cat) => (
+        <CatItem img={cat} key={cat} />
+      ))}
+    </ul>
+  );
+}
+
+
+const App = () => {
+  const CAT1 =
+    "https://cataas.com/cat/HSENVDU4ZMqy7KQ0/says/react";
+  const CAT2 =
+    "https://cataas.com/cat/BxqL2EjFmtxDkAm2/says/inflearn";
+  const CAT3 =
+    "https://cataas.com/cat/18MD6byVC1yKGpXp/says/JavaScript";
+
+  const [counter, setCounter] = React.useState(() => {
+    return jsonLocalStorage.getItem("counter");
+  });
+  const [mainCat, setMainCat] = React.useState(CAT1);
+  const [favorites, setFavorites] = React.useState(() => {
+    return jsonLocalStorage.getItem("favorites") || [];
+  });
+
+  const alreadFavorite = favorites.includes(mainCat)
+
+  async function setInitialCat() {
+    const newCat = await fetchCat('first cat');
+    setMainCat(newCat);
+  }
+
+  React.useEffect(() => {
+    setInitialCat();
+  }, []);
+
+  React.useEffect(() => {
+    console.log("카운터 증가");
+  }, [counter]);
+
+
+  async function updateMainCat(value) {
+    const newCat = await fetchCat(value);
+
+    setMainCat(newCat);
+
+    setCounter((prev) => {
+      const nextCounter = prev + 1;
+      jsonLocalStorage.setItem("counter", nextCounter);
+      return nextCounter;
+    });
+  }
+
+  function handleHeartClick() {
+    const nextFavorites = [...favorites, mainCat];
+    setFavorites(nextFavorites);
+    jsonLocalStorage.setItem("favorites", nextFavorites);
+  }
+
+  const conunterTitle = counter === null ? '' : counter + "번째 ";
+
+  return (
+    <div>
+      <Title>{conunterTitle}고양이 가라사대</Title>
+      <Form updateMainCat={updateMainCat} />
+      <MainCard img={mainCat} onHeartClick={handleHeartClick} alreadFavorite={alreadFavorite} />
+      <Favorites favorites={favorites} />
+    </div>
+  );
+};
 
 export default App;
